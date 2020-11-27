fbb67a701dabdab23c05556c424f9bb0865d6ed9
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 00:37:59 +0530

    Create P1.java

---
+++ f2/P1.java
@@ -0,0 +1,8 @@
+public class Sample{
+    char x = 'a';
+    public static void main (String[] args) {
+        Sample sample = new Sample();
+        //this will print the ascii value of x(char) 
+        System.out.println((int)sample.x);
+    }
+}
