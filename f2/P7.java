c80efcfb31a95cb78fb08c6b2cdd17e66978f27c
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 01:22:04 +0530

    Create P7.java

---
+++ f2/P7.java
@@ -0,0 +1,14 @@
+public class Point {
+    int x=1;
+    int y=2;
+}
+public class Sample {
+    int z=3;
+    Point p=new Point(); 
+    public static void main(String[] args) { 
+        Sample s=new Sample();
+        System.out.println(s.p.x);
+        System.out.println(s.p.y);	
+        System.out.println(s.z);
+    }
+}
