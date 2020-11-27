04daf5e7491dbc4c44b263055aa3fb80bd39aff7
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 00:40:12 +0530

    Create P2.java

---
+++ f2/P2.java
@@ -0,0 +1,10 @@
+public class Sample{
+    int x;
+    int y;
+    public static void main (String[] args) {
+        Sample sample = new Sample();
+        //this will print the default value of integer data type.
+        System.out.println(sample.x);
+        System.out.println(sample.y);
+    }
+}
