be51bfb8f4a08516303530d4ec154f681303e3c1
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:53:15 +0530

    Create P16.java

---
+++ f5/P16.java
@@ -0,0 +1,10 @@
+class Sample{
+  public static void main(String[] args){
+    char y=10;
+    byte x=(byte)y;
+    
+    System.out.println(x);
+  }
+}
+
+// https://ide.geeksforgeeks.org/Gr21roY8t9
