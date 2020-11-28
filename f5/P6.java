5763843eb3326f25f552fa23e245b2420020de2f
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:30:11 +0530

    Create P6.java

---
+++ f5/P6.java
@@ -0,0 +1,15 @@
+class Demo{
+  private Demo(){
+
+  }
+}
+class Point{
+  protected Point(){
+
+  } 
+  public static void main (String[] args) {
+      /* code */
+  }
+} 
+
+//https://ide.geeksforgeeks.org/cbTBfIe4Rl
