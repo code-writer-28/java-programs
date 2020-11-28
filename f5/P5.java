2cd14927d0871d23c6bbc52996ace6366bb946ab
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:27:50 +0530

    Create P5.java

---
+++ f5/P5.java
@@ -0,0 +1,13 @@
+class Demo{
+
+}
+class Point{
+  Point(int x,int y){
+    System.out.println("CB-POINT-2");
+  }
+  public static void main (String[] args) {
+      /* code */
+  }
+} 
+
+//https://ide.geeksforgeeks.org/CBP6M1opSd
