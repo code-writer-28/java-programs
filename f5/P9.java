2b0947c4faa75ce8472db1b65eff796aa68fd36b
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:36:16 +0530

    Create P9.java

---
+++ f5/P9.java
@@ -0,0 +1,8 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println(Byte.MIN_VALUE);
+    System.out.println(Byte.MAX_VALUE);
+  }
+}
+
+//https://ide.geeksforgeeks.org/QsSddmYbHk
