8ec9ed049c5259e2ecd55a9cfe72183704463198
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:47:02 +0530

    Create P11.java

---
+++ f5/P11.java
@@ -0,0 +1,8 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println((int)Character.MIN_VALUE);
+    System.out.println((int)Character.MAX_VALUE);
+  }
+}
+
+// https://ide.geeksforgeeks.org/fgk3VmQ9d6
