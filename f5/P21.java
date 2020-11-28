4ab0613268876bf867c60374386b14396d51aabe
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 18:06:01 +0530

    Create P21.java

---
+++ f5/P21.java
@@ -0,0 +1,13 @@
+class Sample{
+  public static void main(String[] args){
+    String s1="ABC";
+    String s2=new String("ABC");
+    String s3="ABC";
+    String s4=new String("ABC");
+    System.out.println(s1==s2);
+    System.out.println(s1==s3);
+    System.out.println(s2==s4);
+  }
+}
+
+//https://ide.geeksforgeeks.org/CMZFsoNXTH
