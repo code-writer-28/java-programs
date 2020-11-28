6dbcca865549e0a68d62496447f9f95a192544c1
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:49:55 +0530

    Create P14.java

---
+++ f5/P14.java
@@ -0,0 +1,8 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println(Double.MIN_VALUE);
+    System.out.println(Double.MAX_VALUE);
+  }
+}
+
+// https://ide.geeksforgeeks.org/4l2dXIy4Pz
