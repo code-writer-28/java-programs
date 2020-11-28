dadccadf910db7c7e549af714df2828cb0f580e2
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 15:31:04 +0530

    Create P6.java

---
+++ f3/P6.java
@@ -0,0 +1,13 @@
+class Point{
+  static int x=1;
+  static int y=2;
+  int p=3;
+  int q=4;
+} 
+class Sample { 
+  public static void main(String[] args) { 
+    Point p1=new Point();
+    Point p2=new Point(); 
+  }
+}
+//Nothing will print
