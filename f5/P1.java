2f95f99f4bbfb86cd2fa737fb49549f7d9544f17
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:02:54 +0530

    Create P1.java

---
+++ f5/P1.java
@@ -0,0 +1,19 @@
+class Point{
+  static{
+    System.out.println("SB-POINT");
+  }
+  {
+    System.out.println("NSB-POINT");
+  }
+}
+class Sample{
+  public static void main(String[] args){
+    Point p1=new Point();
+    Point p2=new Point();
+
+  }
+}
+
+/*
+ *https://ide.geeksforgeeks.org/qGP8bME0tz
+ */
