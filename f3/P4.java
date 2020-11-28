d24f0b87a2765fc4367234ace10c5946317c645f
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 15:25:20 +0530

    Create P4.java

---
+++ f3/P4.java
@@ -0,0 +1,10 @@
+class Point{
+  static int x=1;
+  static int y=2;
+}
+class Sample { 
+  public static void main(String[] args) { 
+    System.out.println(Point.x);
+    System.out.println(Point.y);
+  }
+}
