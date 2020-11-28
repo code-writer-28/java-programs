9dea5979a9471ae8e522aca9a5839a749a67a10f
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 17:48:00 +0530

    Create P12.java

---
+++ f5/P12.java
@@ -0,0 +1,8 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+  }
+}
+
+// https://ide.geeksforgeeks.org/kS3ElJl7sI
