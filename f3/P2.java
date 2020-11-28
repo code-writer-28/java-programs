97e758d1797a6c924c1584244dd319909c488ce3
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 15:17:58 +0530

    Create P2.java

---
+++ f3/P2.java
@@ -0,0 +1,10 @@
+public class Sample{
+    int x = 1;
+    int y = 2;
+    public static void main (String[] args) {
+        Sample s;
+        s = new Sample();
+        System.out.println(s.x);
+        System.out.println(s.y);
+    }
+}
