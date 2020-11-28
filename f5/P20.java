bbc741e9ac9b8b56211f15d1f95d17c1692c0827
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-28 18:02:36 +0530

    Create P20.java

---
+++ f5/P20.java
@@ -0,0 +1,11 @@
+class Sample{
+  public static void main(String[] args){
+    String s1="ABC";
+    String s2="XYZ";
+    String s3="ABC";
+    System.out.println(s1==s2);
+    System.out.println(s1==s3);
+  }
+}
+
+//https://ide.geeksforgeeks.org/my6HQD29Kb
