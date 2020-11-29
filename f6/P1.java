60fdf3d6e8c852616e93ec6ca6c236db5ad69b18
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 12:22:06 +0530

    Create P1.java

---
+++ f6/P1.java
@@ -0,0 +1,10 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println(10+"ABC");
+    System.out.println("ABC"+10);
+    System.out.println("ABC"+"XYZ");
+
+  }
+}
+
+//https://ide.geeksforgeeks.org/7ZOEa7dg2P
