9333e1865a8d3b639aeab13de3988f6f8aa6edc3
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 13:02:30 +0530

    Create P7.java

---
+++ f6/P7.java
@@ -0,0 +1,18 @@
+class A{}
+class B{}
+class C{}
+class D{}
+
+class Sample{
+  public static void main(String[] args){
+    A r1=new A();
+    B r2=new B();
+    Sample r3=new Sample();
+    System.out.println(r1); 
+    System.out.println(r2);
+    System.out.println(r3);
+  }
+}
+
+// https://ide.geeksforgeeks.org/IUyCV3ydSm
+// class name with hashcode
