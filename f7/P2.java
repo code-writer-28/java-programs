bca51e9fe43f01bcdc31de0e25fb6cd208e0967e
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 17:16:55 +0530

    Create P2.java

---
+++ f7/P2.java
@@ -0,0 +1,12 @@
+class Sample{
+  public static void main(String[] args){
+    int x=10;
+    Integer y=new Integer(x); //boxing
+    Integer z=x;              //auto-boxing
+    
+    System.out.println(y);
+    System.out.println(z);
+  }
+}
+
+//https://ide.geeksforgeeks.org/ch7qAXWxly
