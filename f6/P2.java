5c4a63942ade85db34179bb270f1d36f461b0cad
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 12:27:21 +0530

    Create P2.java

---
+++ f6/P2.java
@@ -0,0 +1,12 @@
+ class Sample{
+  public static void main(String[] args){
+    int x=10,y=20;
+    System.out.println(x+"ABC"); 
+    System.out.println(x+",");
+    System.out.println(","+y);
+    System.out.println(x+" "+y);
+  }
+}
+
+// https://ide.geeksforgeeks.org/H0UbQMUb9r
+  
