128dc949b95a6100debbf6e0b5d8b942d77c4ed8
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 13:03:58 +0530

    Create P8.java

---
+++ f6/P8.java
@@ -0,0 +1,20 @@
+class A{}
+class B{}
+class C{}
+class D{}
+
+class Sample{
+  public static void main(String[] args){
+    A r1=new A();
+    B r2=new B();
+    Sample r3=new Sample();
+    System.out.println(r1);  //sop(r1.toString())
+    System.out.println(r2);  //sop(r2.toString())
+    System.out.println(r3);  //sop(r3.toString())
+  }
+}
+
+// https://ide.geeksforgeeks.org/HYjkpjoiF1
+
+
+  
