32b55f071e27558b24831e000251ce13067f4c80
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 13:04:43 +0530

    Create P9.java

---
+++ f6/P9.java
@@ -0,0 +1,21 @@
+class A{}
+class B{}
+class C{}
+class D{}
+
+class Sample{
+  public static void main(String[] args){
+    A r1=new A();
+    B r2=new B();
+    Sample r3=new Sample();
+    System.out.println(r1);  
+    System.out.println(r1.toString());
+    System.out.println(r2);  
+    System.out.println(r2.toString());
+    System.out.println(r3);  
+    System.out.println(r3.toString());
+  }
+}
+
+//https://ide.geeksforgeeks.org/604ywXq2Oi
+  
