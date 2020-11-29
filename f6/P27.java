6a1f8a899cb04d29ae0940964911c239b82b0783
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 16:03:47 +0530

    Create P27.java

---
+++ f6/P27.java
@@ -0,0 +1,19 @@
+class Point{
+   int x;
+   int y; 
+   Point(int x,int b){
+
+   }
+   void display(){ 
+     System.out.println(x);  
+     System.out.println(y);   
+   } 
+}
+class Sample{
+  public static void main(String[] args){
+    Point p1=new Point(10,20);
+    p1.display();
+  }
+}
+
+//https://ide.geeksforgeeks.org/qQX76huycd
