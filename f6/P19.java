d975cfb5fb8032914a36bd31bb2b368e339e63b8
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 15:23:16 +0530

    Create P19.java

---
+++ f6/P19.java
@@ -0,0 +1,11 @@
+class Numeric{
+  int x;
+}
+class Sample{
+  public static void main(String[] args){
+    Numeric n1=new Numeric(); 
+    n1=new Numeric();
+  }
+}
+
+//https://ide.geeksforgeeks.org/hhqsok7tS1
