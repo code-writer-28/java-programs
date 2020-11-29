c898576417b4e57a7670212bc3015342c81003b0
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 13:07:23 +0530

    Create P11.java

---
+++ f6/P11.java
@@ -0,0 +1,24 @@
+class A
+{
+  public String toString(){
+    return "HELLO";
+  }
+}
+class B{
+  public String toString(){
+    return "INDORE";
+  }
+}
+class Sample{
+  public static void main(String[] args){
+    A r1=new A();
+    A r2=new A();
+    B r3=new B();
+    System.out.println(r1); 
+    System.out.println(r2); 
+    System.out.println(r3);
+  }
+}
+
+//https://ide.geeksforgeeks.org/rxRxU0ycBY
+  
