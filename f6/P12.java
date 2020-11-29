e08daa50ded99a935047b3199245f27984072e2f
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 13:08:26 +0530

    Create P12.java

---
+++ f6/P12.java
@@ -0,0 +1,12 @@
+class Numeric{
+  int x;
+}
+class Sample{
+  public static void main(String[] args){
+    Numeric n1=new Numeric();
+    System.out.println(n1.x);
+  }
+}
+
+
+//https://ide.geeksforgeeks.org/vOgAiVHhtb 
