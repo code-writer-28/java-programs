0c6b8db5a295b3551fe88187499408a17f1dd985
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2020-11-29 12:29:33 +0530

    Create P3.java

---
+++ f6/P3.java
@@ -0,0 +1,8 @@
+class Sample{
+  public static void main(String[] args){
+    System.out.println(10+""+20); 
+    System.out.println(10+20+"ABC"+30+40);    
+  }
+}
+
+// https://ide.geeksforgeeks.org/BFXZ0Q6Phm
